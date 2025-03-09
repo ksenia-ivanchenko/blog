d2bf075742b92678e721fa455eb9dd7b77ae7a3f
Ksenia Ivanchenko <k.ivanchenko20@gmail.com>
2025-03-09 17:17:15 +0700

    L2-6 добавлен компонент кнопки

--- src/shared/ui/button/button.tsx
+++ src/shared/ui/button/button.tsx
@@ -1,1 +1,13 @@
-export const Button = () => <></>;
+import { ComponentProps } from 'react';
+import styles from './button.module.scss';
+import clsx from 'clsx';
+
+interface ButtonProps extends ComponentProps<'button'> {
+  className?: string;
+}
+
+export const Button = ({ children, className, ...props }: ButtonProps) => (
+  <button className={clsx(styles.button, className)} {...props}>
+    {children}
+  </button>
+);
