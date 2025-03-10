0d8e05678e134739c8a0f2404096fce08d781393
Ksenia Ivanchenko <k.ivanchenko20@gmail.com>
2025-03-10 15:11:49 +0700

    L2-9 добавлена главная страница

--- src/pages/main/ui/main-page.tsx
+++ src/pages/main/ui/main-page.tsx
@@ -1,1 +1,7 @@
-export const MainPage = () => <></>;
+import styles from './main-page.module.scss';
+
+export const MainPage = () => (
+  <div className={styles.page}>
+    <h1 className={styles.heading}>Главная</h1>
+  </div>
+);
