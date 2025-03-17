00767ced3854d5fc1ddc916a67f6a5cda3a9058a
Ksenia Ivanchenko <k.ivanchenko20@gmail.com>
2025-03-17 15:14:24 +0700

    L3-3 добавлена ссылка на /landing в хедере

--- src/widgets/header/helpers/getTabsRoutes.ts
+++ src/widgets/header/helpers/getTabsRoutes.ts
@@ -4,5 +4,6 @@ export const getTabsRoutes = () => {
   return [
     { path: routes.main.getLink(), name: 'Главная' },
     { path: routes.randomPost.getLink(), name: 'Рандомный пост' },
+    { path: routes.landing.getLink(), name: 'Landing' },
   ];
 };
